e2b8b06ec4b6c45e86a294675e0372d52129bf4a
KthrnG <kgedrat@hfk-bremen.de>
2019-07-27 13:17:24 +0200

    MySQL Config aufräumen

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
 session_start();
 		$servername = "localhost";
 		$username = "root";//hier benutzernamen eintragen
-		$password = "h24w1996";//hier passwort eintragen
+		$password = "";//hier passwort eintragen
 		$dbname = "webseitewapp";//hier Datenbanknamen eintragen
 
 		// Create connection
