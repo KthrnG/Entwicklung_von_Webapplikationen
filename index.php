d38d148b7b14a10e501b78c85c714b154fb7d23f
KthrnG <kgedrat@hfk-bremen.de>
2019-07-27 13:16:47 +0200

    Kosmetik

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <!--image-->
     <div class="background">
         <!--title here so it isn't transparent-->
-        <h1 class="ontop"> Das Kräuterarchiv </h1>
+        <h1 class="ontop">Das Kräuterarchiv</h1>
         <a href="index.php">
             <!-- transparent box-->
             <div class="transbox"></div>
