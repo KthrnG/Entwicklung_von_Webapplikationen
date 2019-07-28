254a9ff4760622e011cd8cf9cd149a11b9fe4fc8
kthrnG <kgedrat@hfk-bremen.de>
2019-07-28 17:58:34 +0200

    Bewertung von Kräutern einbauen

--- detail.php
+++ detail.php
@@ -18,41 +18,43 @@ include "includes/navigationBar.php";
 ?>
 
 <?php
-$sql = "SELECT medium.name, datei.adr, medium.id, medium.beschreibung, medium.erntezeit, medium.standort, medium.latein_name, medium.aussaat FROM (medium INNER JOIN datei ON medium.id = datei.medium_id) WHERE medium.id = $_GET[id]";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) != 1) {
-    header("Location:notfound.php");
-    exit();
+if (isset($_GET["id"])) {
+    $sql = "SELECT medium.name, datei.adr, medium.id, medium.beschreibung, medium.erntezeit, medium.standort, medium.latein_name, medium.aussaat FROM (medium INNER JOIN datei ON medium.id = datei.medium_id) WHERE medium.id = $_GET[id]";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) != 1) {
+        header("Location:notfound.php");
+        exit();
+    }
+    $row = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
 }
-$row = mysqli_fetch_assoc($result);
-mysqli_free_result($result);
 ?>
 
 <div class="plantbox">
     <div class="relative">
         <img class="image" src="<?php echo $row["adr"] ?>" alt="<?php echo $row["name"] ?>"/>
-        <div id="thumb"></div>
-        <div id="heart"></div>
+        <?php include "includes/bewertung.php";?>
+        <div class="heart"></div>
     </div>
 
     <div class="descrBox">
         <div class="steckbrief">
-            <h2><?php echo $row["name"]?></h2>
-            <p><?php echo $row["latein_name"]?></p>
+            <h2><?php echo $row["name"] ?></h2>
+            <p><?php echo $row["latein_name"] ?></p>
             <table>
-                <caption>Merkmale von <?php echo $row["name"]?></caption>
+                <caption>Merkmale von <?php echo $row["name"] ?></caption>
                 <tbody>
                 <tr>
                     <td>Standort</td>
-                    <td><?php echo $row["standort"]?></td>
+                    <td><?php echo $row["standort"] ?></td>
                 </tr>
                 <tr>
                     <td>Aussaat</td>
-                    <td><?php echo $row["aussaat"]?></td>
+                    <td><?php echo $row["aussaat"] ?></td>
                 </tr>
                 <tr>
                     <td>Erntezeit</td>
-                    <td><?php echo $row["erntezeit"]?></td>
+                    <td><?php echo $row["erntezeit"] ?></td>
                 </tr>
                 </tbody>
             </table>
@@ -63,7 +65,7 @@ mysqli_free_result($result);
                 <caption>Beschreibung</caption>
                 <tbody>
                 <tr>
-                    <td><?php echo $row["beschreibung"]?>
+                    <td><?php echo $row["beschreibung"] ?>
                     </td>
                 </tr>
                 </tbody>
---
+++ includes/bewertung.php
@@ -0,0 +1,53 @@
+<?php
+if (isset($_POST["like"])) {
+    if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
+        $error = "Bitte melde dich an, um eine Bewertung abzugeben.";
+    } else {
+        $ins = "INSERT INTO bewertung (`medium_id`, `user_id`, `wert`) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($ins);
+        $stmt->bind_param("iii", $_GET['id'], $_SESSION['uid'], $_POST['like']);
+        if (!$stmt->execute()) {
+            $error = "Fehler beim Speichern der Bewertung";
+        }
+    }
+}
+
+if (isset($_GET['id'])) {
+    $avg_sql = "SELECT AVG(wert) average FROM bewertung WHERE medium_id = $_GET[id]";
+    $avg_res = mysqli_query($conn, $avg_sql);
+    if (mysqli_num_rows($avg_res) != 1) {
+        $average_rating = 0;
+    } else {
+        $avg_row = mysqli_fetch_assoc($avg_res);
+        $average_rating = number_format($avg_row['average'], 1, '.', '');
+        mysqli_free_result($avg_res);
+    }
+}
+
+$button_disabled = "";
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
+    $rating_sql = "SELECT wert FROM bewertung WHERE medium_id = $_GET[id] AND user_id = $_SESSION[uid]";
+    $rating_res = mysqli_query($conn, $rating_sql);
+    if (mysqli_num_rows($rating_res) > 0) {
+        $button_disabled = "disabled";
+    } else {
+        $button_disabled = "";
+    }
+}
+?>
+<?php if (isset($error)) {
+    echo '<div class="error_msg">' . $error . '</div>';
+}
+?>
+<div class="thumb">
+    <form action="" method="post">
+        <button type="submit" name="like" value="1" <?php echo $button_disabled?>><i class="far fa-thumbs-up"></i></button>
+        <button type="submit" name="like" value="2" <?php echo $button_disabled?>><i class="far fa-thumbs-up"></i></button>
+        <button type="submit" name="like" value="3" <?php echo $button_disabled?>><i class="far fa-thumbs-up"></i></button>
+        <button type="submit" name="like" value="4" <?php echo $button_disabled?>><i class="far fa-thumbs-up"></i></button>
+        <button type="submit" name="like" value="5" <?php echo $button_disabled?>><i class="far fa-thumbs-up"></i></button>
+    </form>
+</div>
+&Oslash; <?php echo $average_rating ?>
+
