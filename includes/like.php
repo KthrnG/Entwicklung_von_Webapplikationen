82c07b2e552b3aeeed65c9980c76a11d9c5b5698
kthrnG <kgedrat@hfk-bremen.de>
2019-07-30 19:55:57 +0200

    Kräuter von der Wunschliste entfernen

--- includes/like.php
+++ includes/like.php
@@ -7,18 +7,24 @@ if (isset($_POST["like"])) {
         $stmt = $conn->prepare($ins);
         $stmt->bind_param("ii", $_GET['id'], $_SESSION['uid']);
         if (!$stmt->execute()) {
-            $like_error = "Fehler beim Speichern der Bewertung";
+            $like_error = "Fehler beim Speichern der Wunschliste";
         }
     }
 }
 
-$like_button_disabled = "";
+if (isset($_POST["unlike"])) {
+    $del = "DELETE FROM wunschliste WHERE medium_id = $_GET[id] AND user_id = $_SESSION[uid]";
+    $stmt = $conn->prepare($del);
+    if (!$stmt->execute()) {
+        $like_error = "Fehler beim Löschen von der Wunschliste";
+    }
+}
 
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
     $like_sql = "SELECT user_id FROM wunschliste WHERE medium_id = $_GET[id] AND user_id = $_SESSION[uid]";
     $like_res = mysqli_query($conn, $like_sql);
     if (mysqli_num_rows($like_res) > 0) {
-        $like_button_disabled = "disabled";
+        $liked = true;
     }
 }
 ?>
@@ -28,6 +34,9 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
 ?>
 <div class="heart">
     <form action="" method="post">
-        <button type="submit" name="like" <?php echo $like_button_disabled ?>><i class="fas fa-heart"></i></i></button>
+        <button type="submit"
+            <?php if (isset($liked) && $liked) echo 'class="active" name="unlike"'; else echo 'name="like"'; ?>>
+            <i class="fas fa-heart"></i></i>
+        </button>
     </form>
 </div>
