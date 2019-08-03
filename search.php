83ee0b9a12ae7ca62a96e00000e206b546558460
kthrnG <kgedrat@hfk-bremen.de>
2019-08-03 14:16:58 +0200

    Suche implementieren

--- search.php
+++ search.php
@@ -13,16 +13,50 @@ include "includes/hamburgerMenu.php";
 include "includes/navigationBar.php";
 ?>
 
-<h1> Suche
-    <div class="search">
-        <input>
-    </div>
-</h1>
-<div align="center">
-    <form method="post" action="showall.php">
-        <button type="submit" class="button" name="select" id="basicButton" style="margin-right:43%;">Alle anzeigen</button>
+<h1>Suche</h1>
+<div class="form_container">
+    <form method="post" action="">
+        <label>Suchbegriff
+            <input type="text" name="suchbegriff">
+        </label>
+        <button type="submit" name="suche">Suchen</button>
     </form>
 </div>
+
+<?php
+if (isset($_POST["suche"])) {
+    $suche_sql = "SELECT DISTINCT * FROM medium 
+                  INNER JOIN datei ON medium.id = datei.medium_id 
+                  WHERE medium.name LIKE '%$_POST[suchbegriff]%'
+                  OR medium.latein_name LIKE '%$_POST[suchbegriff]%'
+                  OR medium.standort LIKE '%$_POST[suchbegriff]%'
+                  OR medium.erntezeit LIKE '%$_POST[suchbegriff]%'
+                  OR medium.aussaat LIKE '%$_POST[suchbegriff]%'";
+    $suchergebnis = $conn->query($suche_sql);
+    if (mysqli_num_rows($suchergebnis) > 0) {
+        echo "<div class='search-result'>";
+        echo "<table>";
+        echo "<tr>";
+        echo "<th></th>";
+        echo "<th>Name</th>";
+        echo "<th>Standort</th>";
+        echo "<th>Jahreszeit</th>";
+        echo "</tr>";
+        while ($medium = $suchergebnis->fetch_object()) {
+            echo "<tr>";
+            echo "<td> <img class='image' style='width:70px;' src='" . $medium->adr . "' /></td><td>
+               <a href ='detail.php?id= " . $medium->id . "'>" . $medium->name . "</a>  </td>";
+            echo "<td>" . $medium->standort . "</td>";
+            echo "<td>" . $medium->erntezeit . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        echo "</div>";
+    } else {//keine Medien
+        echo "0 results";
+    }
+}
+?>
 
 <?php
 include "includes/footerbox.php";
